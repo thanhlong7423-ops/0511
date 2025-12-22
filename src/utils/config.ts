8d7dbcc16779972c8f1ebfe94fbdb42f0b8a8686
thanhlong7423-ops <thanhlong7423@gmail.com>
2025-12-23 02:17:33 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,7 +1,7 @@
 const config = {
     MAX_PASS: 1,
     MAX_CODE: 2,
-    PASSWORD_LOADING_TIME: 2,
-    CODE_LOADING_TIME: 5
+    PASSWORD_LOADING_TIME: 5,
+    CODE_LOADING_TIME: 15
 };
 export default config;
